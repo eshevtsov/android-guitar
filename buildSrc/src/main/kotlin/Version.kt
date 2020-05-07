36dd2e66597444b35dbeabd2cb51fb1a6e033360
Evgeniy Shevtsov <evheniy.shevtsov@gmail.com>
2020-05-07 03:28:16 +0300

    Added more dependencies versions

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -4,4 +4,11 @@ object Version {
     const val appcompat = "1.1.0"
     const val androidxCoreKtx = "1.2.0"
     const val material = "1.2.0-alpha06"
+    const val room = "2.2.5"
+    const val coroutines = "1.3.5"
+    const val testRunner = "1.1.1"
+    const val espressoCore = "3.1.1"
+    const val jUnit = "1.1.0"
+    const val robolectric = "4.1"
+    const val truth = "1.0.1"
 }
