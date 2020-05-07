363bda6ff3d942da5a3adc27563b1983b2b8fb5d
Evgeniy Shevtsov <evheniy.shevtsov@gmail.com>
2020-05-07 03:28:53 +0300

    Added database module to buildScr

--- buildSrc/src/main/kotlin/Module.kt
+++ buildSrc/src/main/kotlin/Module.kt
@@ -2,5 +2,7 @@ object Module {
     const val coreUi = ":core:uikit"
     const val coreFeature = ":core:feature"
 
+    const val data = ":database"
+
     const val featureSplash = ":feature:splash"
 }
