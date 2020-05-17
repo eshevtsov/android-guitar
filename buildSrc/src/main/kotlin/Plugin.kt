3ef293955a681a79c1253b2b9278c3869f8df68b
Evgeniy Shevtsov <evheniy.shevtsov@gmail.com>
2020-05-17 15:13:16 +0300

    Added more buildSrc plugins

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,5 +1,6 @@
 object Plugin {
     const val gradle = "com.android.tools.build:gradle:${Version.gradlePlugin}"
+    const val googleServices = "com.google.gms:google-services:${Version.googleServicesPlugin}"
     const val testRunner = "androidx.test.runner.AndroidJUnitRunner"
     const val navigationSafeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:${Version.navigation}"
 }
