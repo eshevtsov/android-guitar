5a3d7c92aaa2fbc95e51937fa6aab6dbb0641ebd
Evgeniy Shevtsov <evheniy.shevtsov@gmail.com>
2020-05-24 03:34:08 +0300

    Configured string provider koin dependency injection

--- app/src/main/java/com/eshevtsov/android/guitar/assistant/koin/app/AppModule.kt
+++ app/src/main/java/com/eshevtsov/android/guitar/assistant/koin/app/AppModule.kt
@@ -3,6 +3,8 @@ package com.eshevtsov.android.guitar.assistant.koin.app
 import com.eshevtsov.android.guitar.assistant.MainFragment
 import com.eshevtsov.android.guitar.assistant.core.feature.logger.AndroidLogger
 import com.eshevtsov.android.guitar.assistant.core.feature.logger.Logger
+import com.eshevtsov.android.guitar.assistant.core.feature.resource.AndroidStringProvider
+import com.eshevtsov.android.guitar.assistant.core.feature.resource.StringProvider
 import com.eshevtsov.android.guitar.assistant.data.AppPreferences
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.fragment.dsl.fragment
@@ -11,6 +13,7 @@ import org.koin.dsl.module
 object AppModule {
     val default = module {
         single<Logger> { AndroidLogger }
+        single<StringProvider> { AndroidStringProvider(androidContext().resources) }
         single { AppPreferences(androidContext()) }
         single { get<AppPreferences>().preferencesValue }
         fragment { MainFragment(get()) }
